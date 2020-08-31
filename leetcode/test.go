0d395539cb9d7c9527f2fd1caa024a1e7eba28d3
Tany <1173904005@qq.com>
2020-08-31 18:08:37 +0800

    leetcode finished 39题 回溯+剪枝

--- leetcode/test.go
+++ leetcode/test.go
@@ -1,9 +1,7 @@
 package leetcode
 
-import "fmt"
-
 //测试
 func TestAlgor() {
-	nums := []int{1, 3, 5, 6}
-	fmt.Println(searchInsert(nums, 5))
+	// nums := []int{2, 3, 5}
+	// fmt.Println(combinationSum(nums, 8))
 }
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"algor/leetcode"
+	"fmt"
 )
 
 func main() {
@@ -23,5 +24,17 @@ func debug() {
 }
 
 func test() {
-
+	items := []int{1, 2, 3, 4, 5}
+	doubleSlice1 := make([][]int, 1)
+	doubleSlice2 := make([][]int, 1)
+	singleSlice := items[1:3]
+	doubleSlice1[0] = items[2:3]
+	doubleSlice2[0] = items[1:3]
+	items[2] = 999
+	fmt.Println(items)
+	fmt.Println(singleSlice)
+	fmt.Println(doubleSlice1)
+	fmt.Printf("singleSlice:%p,%p\n", &singleSlice, singleSlice)
+	fmt.Printf("doubleSlice1:%p,%p\n", &doubleSlice1[0], doubleSlice1[0])
+	fmt.Printf("doubleSlice2:%p,%p\n", &doubleSlice2[0], doubleSlice2[0])
 }
