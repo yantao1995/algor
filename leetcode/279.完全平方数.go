c0c7c683ef31ab716131eff7879d41f7f51378c1
Tany <1173904005@qq.com>
2022-03-25 23:21:49 +0800

    1

--- leetcode/279.完全平方数.go
+++ leetcode/279.完全平方数.go
@@ -1,9 +1,5 @@
 package leetcode
 
-import (
-	"math"
-)
-
 /*
  * @lc app=leetcode.cn id=279 lang=golang
  *
@@ -12,18 +8,16 @@ import (
 
 // @lc code=start
 func numSquares(n int) int {
-	sqrtIndex := make([]int, int(math.Sqrt(float64(n)))+1)
-	for i := 1; i < len(sqrtIndex); i++ {
-		sqrtIndex[i] = i * i
+	memo := make([]int, n+1)
+	for i := 1; i*i <= n; i++ {
+		memo[i*i] = 1
 	}
-	minCount := 1
 	min := func(a, b int) int {
 		if a < b {
 			return a
 		}
 		return b
 	}
-	memo := make([]int, n+1)
 	var bfs func(n int) int
 	bfs = func(n int) int {
 		if n < 4 {
@@ -33,18 +27,15 @@ func numSquares(n int) int {
 		if memo[n] > 0 {
 			return memo[n]
 		}
-		minCount := n
-		for i := len(sqrtIndex); i > 0; i-- {
-			if n > sqrtIndex[i] {
-				minCount = min(minCount, bfs(sqrtIndex[i])+bfs(n-sqrtIndex[i]))
-			} else if n == sqrtIndex[i] {
-				minCount = 1
-			}
+		m := n
+		for i := 1; i <= n/2; i++ {
+			m = min(m, bfs(n-i)+bfs(i))
 		}
-		memo[n] = minCount
-		return minCount
+		memo[n] = m
+		return m
 	}
-	return minCount
+	bfs(n)
+	return memo[n]
 }
 
 // @lc code=end
