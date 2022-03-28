e93667fe8af8a50aa782ba9adcc0b517ec7dd63b
Tany <1173904005@qq.com>
2022-03-28 19:27:00 +0800

    1

--- leetcode/array/152.乘积最大子数组.go
+++ leetcode/array/152.乘积最大子数组.go
@@ -9,11 +9,16 @@ package leetcode
 // @lc code=start
 func maxProduct(nums []int) int {
 	max := nums[0]
-	dp := make([][]int, len(nums))
 	for i := 0; i < len(nums); i++ {
-
+		current := 1
 		for j := i; j < len(nums); j++ {
-
+			current *= nums[j]
+			if current > max {
+				max = current
+			}
+			if nums[j] == 0 {
+				break
+			}
 		}
 	}
 	return max
---
+++ leetcode/linked_list/142.环形链表-ii.go
@@ -0,0 +1,29 @@
+package leetcode
+
+/*
+ * @lc app=leetcode.cn id=142 lang=golang
+ *
+ * [142] 环形链表 II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func detectCycle(head *ListNode) *ListNode {
+	m := map[*ListNode]bool{}
+	for head != nil {
+		if m[head] {
+			return head
+		}
+		m[head] = true
+		head = head.Next
+	}
+	return nil
+}
+
+// @lc code=end
