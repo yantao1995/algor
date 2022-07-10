6548c2557bcb9a790718426308151995f77bd6bd
Tany <1173904005@qq.com>
2022-07-10 22:01:24 +0800

    1

---
+++ leetcode/741.摘樱桃.go
@@ -0,0 +1,14 @@
+package leetcode
+
+/*
+ * @lc app=leetcode.cn id=741 lang=golang
+ *
+ * [741] 摘樱桃
+ */
+
+// @lc code=start
+func cherryPickup(grid [][]int) int {
+	return 0
+}
+
+// @lc code=end
