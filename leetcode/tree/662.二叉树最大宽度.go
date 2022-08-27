063ea399127a4a4ec369b5c2be95ddf82a3f8a18
Tany <1173904005@qq.com>
2022-08-27 10:52:35 +0800

    1

--- leetcode/tree/662.二叉树最大宽度.go
+++ leetcode/tree/662.二叉树最大宽度.go
@@ -16,8 +16,8 @@ package leetcode
  * }
  */
 func widthOfBinaryTree(root *TreeNode) int {
-	levelMin, levelMax := map[int]int{}, map[int]int{}
-	maxWidth := 0
+	levelMin := map[int]int{}
+	maxWidth := 1
 	max := func(a, b int) int {
 		if a > b {
 			return a
@@ -29,13 +29,11 @@ func widthOfBinaryTree(root *TreeNode) int {
 		if root != nil {
 			if levelMin[level] == 0 || rank < levelMin[level] {
 				levelMin[level] = rank
-			}
-			if levelMax[level] == 0 || rank > levelMax[level] {
-				levelMax[level] = rank
+			} else {
+				maxWidth = max(maxWidth, rank-levelMin[level]+1)
 			}
 			getLevelLocation(root.Left, level+1, rank*2-1)
 			getLevelLocation(root.Right, level+1, rank*2)
-			maxWidth = max(maxWidth, levelMax[level]-levelMin[level]+1)
 		}
 	}
 	getLevelLocation(root, 0, 1)
@@ -48,5 +46,6 @@ func widthOfBinaryTree(root *TreeNode) int {
 	因为结构视作满二叉树，所以父节点的层排位，与子节点的层排位关系为
 		childLeft =  parent * 2 -1
 		childRight =  parent *2
-	那么可以存储 levelMin, levelMax 每一层的最小rank和最大rank,然后与maxWidth比较即可
+	//那么可以存储 levelMin, levelMax 每一层的最小rank和最大rank,然后与maxWidth比较即可
+	优化 ： 去掉 levelMax ,只要当前值不为最小值，那么就可以进行比较，即当前值减最小值
 */
