bde6bec31beda2e7ea9a0de2f8049af6cda2fe8d
Tany <1173904005@qq.com>
2022-12-08 09:37:03 +0800

    1

---
+++ leetcode/other_easy/1812.判断国际象棋棋盘中一个格子的颜色.go
@@ -0,0 +1,18 @@
+package leetcode
+
+/*
+ * @lc app=leetcode.cn id=1812 lang=golang
+ *
+ * [1812] 判断国际象棋棋盘中一个格子的颜色
+ */
+
+// @lc code=start
+func squareIsWhite(coordinates string) bool {
+	return int(coordinates[0]-'a')%2 != int(coordinates[1]-'1')%2
+}
+
+// @lc code=end
+
+/*
+	直接观察棋盘，行与列对2取余不相等相等的为白色。
+*/
