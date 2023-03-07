d8b3d0a3a47eb76c7731f5ff667b57993a1909cf
Tany <1173904005@qq.com>
2023-03-07 18:01:00 +0800

    stack

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func main() {
 	test2()
 }
@@ -9,5 +14,133 @@ func main() {
 // }
 
 func test2() {
+	s := `11+22*3+4*(60/3+5)/2*(2*(5+15))+{3*[2*(3+2)]}` // = 607
 
+	fmt.Println(rotationCalc(s))
+}
+
+func rotationCalc(s string) string {
+	stack := newStack()
+	for i := 0; i < len(s); i++ {
+		switch s[i] {
+		case '(', '[', '{', '+', '-', '*', '/': //可以直接入栈的符号
+			stack.push(string(s[i])) //转换成 string  入栈
+		case ')', ']', '}': //反括号 需要运算得到括号内的结果
+			find := "{" //需要出栈的符号 遇到对应的符号就终止
+			if s[i] == ')' {
+				find = "("
+			} else if s[i] == ']' {
+				find = "["
+			}
+
+			val := stack.pop()     //括号前一个值
+			operate := stack.pop() //运算符
+			last := stack.pop()    //上一个值
+			current := calc(last, val, operate)
+			stack.pop()         //括号出栈 //TODO 括号嵌套
+			stack.push(current) //当前值入栈
+
+		default: //数字
+			j := i + 1
+			for j < len(s) && s[j] >= '0' && s[j] <= '9' {
+				j++
+			}
+			numStr := s[i:j] // 这个字符串是数字
+			i = j - 1        //移动到数字的下一个索引
+			stack.push(numStr)
+		}
+		//处理当前栈
+		for stack.len() > 2 { //a值  b值 运算符  数量大于3 可以判断操作处理  保证最后只剩一个元素
+			stackLen := stack.len()
+			current := stack.pop()
+			if current != "+" && current != "-" && current != "*" && current != "/" &&
+				current != "(" && current != "[" && current != "{" { //不是运算符
+				operate := stack.pop()
+				if operate == "*" || operate == "/" ||
+					((operate == "+" || operate == "-") && i+1 < len(s) && (s[i+1] == '+' || s[i+1] == '-')) ||
+					i+1 >= len(s) {
+					last := stack.pop()
+					current = calc(last, current, operate)
+				} else {
+					stack.push(operate)
+				}
+			}
+			stack.push(current)
+			if stackLen == stack.len() {
+				break
+			}
+		}
+
+	}
+	fmt.Println(stack.data)
+	return stack.pop()
+}
+
+// 参数分别为 第一个数，第二个数，运算符
+func calc(a, b, operate string) string {
+	aFloat, _ := strconv.ParseFloat(a, 64) //转换成 float
+	bFloat, _ := strconv.ParseFloat(b, 64)
+	result := 0.0
+	switch operate {
+	case "+":
+		result = aFloat + bFloat
+	case "-":
+		result = aFloat - bFloat
+	case "*":
+		result = aFloat * bFloat
+	case "/":
+		result = aFloat / bFloat
+	}
+	return strconv.FormatFloat(result, 'f', 10, 64) //转换成字符串
+}
+
+type stack struct {
+	data []string //用数组表示栈   数组末尾的位置为栈顶
+	ptr  int      //栈顶指针 指向栈顶元素
+}
+
+func newStack() *stack {
+	return &stack{
+		data: []string{},
+		ptr:  -1,
+	}
+}
+
+// 入栈
+func (s *stack) push(x string) {
+	s.ptr++
+	if len(s.data) < s.ptr+1 {
+		s.data = append(s.data, x)
+	}
+	s.data[s.ptr] = x
+}
+
+// 出栈
+func (s *stack) pop() string {
+	if s.len() == 0 {
+		return ""
+	}
+	val := s.data[s.ptr]
+	s.data = s.data[:s.ptr]
+	s.ptr--
+
+	return val
+}
+
+// 获取栈顶元素
+func (s *stack) empty() bool {
+	return s.ptr == -1
+}
+
+// 获取栈顶元素
+func (s *stack) top() string {
+	if s.empty() {
+		return ""
+	}
+	return s.data[s.ptr]
+}
+
+// 获取栈的元素个数
+func (s *stack) len() int {
+	return s.ptr + 1
 }
