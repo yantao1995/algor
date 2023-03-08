df2b9bd9025614b48f5f2b4a020da985435519c2
Tany <1173904005@qq.com>
2023-03-08 22:11:42 +0800

    save

--- main.go
+++ main.go
@@ -40,27 +40,28 @@ var level = map[string]int{
 }
 
 func rotationCalc(slice []string) string {
+	fmt.Println(slice)
 	if len(slice) == 0 {
 		return ""
 	}
 	if level[slice[0]] > 2 { //括号
 		for j := 1; j < len(slice); j++ {
 			if level[slice[j]] == level[slice[0]] { //优先级一样，括号配对了
-				if j >= len(slice) {
-					return rotationCalc(slice[1:j])
-				} else {
-					return calc(rotationCalc(slice[1:j]), slice[j], rotationCalc(slice[j+1:]))
-				}
+				fmt.Println("括号: ", rotationCalc(slice[1:j]), "__", rotationCalc(slice[j+1:]))
+				return calc(rotationCalc(slice[1:j]), slice[j], rotationCalc(slice[j+1:]))
 			}
 		}
-	} else { //是数字
-		if len(slice) > 1 {
-			if 3 >= len(slice) || level[slice[3]] <= level[slice[1]] {
-				slice[2] = calc(slice[0], slice[1], slice[2])
-				return rotationCalc(slice[2:])
-			} else {
-				return calc(slice[0], slice[1], rotationCalc(slice[2:]))
+	} else if len(slice) > 1 { //是数字
+		if len(slice) == 3 || (len(slice) > 3 && level[slice[1]] >= level[slice[3]]) {
+			temp := calc(slice[0], slice[1], slice[2])
+			if len(slice) > 3 {
+				return calc(temp, slice[3], rotationCalc(slice[4:]))
 			}
+			return temp
+		} else {
+			slice[2] = calc(slice[0], slice[1], slice[2])
+			return calc(slice[0], slice[1], rotationCalc(slice[2:]))
+
 		}
 	}
 	return slice[0]
