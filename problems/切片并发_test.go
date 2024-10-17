5af9cd3e1d28303e52835a61fa9bef3ab16dcfcb
yant <yant@wanthings.com>
2024-10-17 10:20:10 +0800

    1

--- problems/切片并发_test.go
+++ problems/切片并发_test.go
@@ -3,6 +3,7 @@ package problems
 import (
 	"encoding/json"
 	"fmt"
+	"testing"
 )
 
 /*
@@ -20,7 +21,8 @@ type P struct {
 	A3 *[]int    //不行
 }
 
-func TestRace() {
+// 并发
+func TestSliceRace(t *testing.T) {
 	p := &P{
 		A0: &[]*[]int{&[]int{1}},
 		A1: &[][]int{[]int{1}},
@@ -59,4 +61,57 @@ func TestRace() {
 			_ = aa[k]
 		}
 	}
+}
+
+// 切片拷贝
+func TestSliceCopy(t *testing.T) {
+	//深拷贝
+	asz := [][1]int{
+		{1},
+		{4},
+	}
+
+	bsz := append([][1]int(nil), asz...)
+	btssz := `[[11],[44]]`
+	json.Unmarshal([]byte(btssz), &asz)
+	fmt.Println(bsz)
+
+	fmt.Println("-----------------------------")
+
+	//浅拷贝
+	a := [][]int{
+		{1},
+		{4},
+	}
+
+	b := append([][]int(nil), a...)
+	bts := `[[11],[44]]`
+	json.Unmarshal([]byte(bts), &a)
+	fmt.Println(b)
+
+	fmt.Println("-----------------------------")
+
+	//单数组深拷贝
+	a1 := []int{
+		1,
+	}
+
+	b1 := append([]int(nil), a1...)
+	a1[0] = 11
+	// bts1 := `[11]`
+	// json.Unmarshal([]byte(bts1), &a1)
+	fmt.Println(b1)
+
+	fmt.Println("-----------------------------")
+
+	//单数组深拷贝
+	a2 := []int{
+		1,
+	}
+	b2 := make([]int, 0, 10)
+	b2 = append(b2, a2...)
+	a2[0] = 11
+	bts2 := `[111]`
+	json.Unmarshal([]byte(bts2), &a2)
+	fmt.Println(a2, b2)
 }
