45ce4755b639bc73bc41410b53b41a160c6054ef
yant <yant@wanthings.com>
2024-10-18 11:12:30 +0800

    1

--- problems/步长精度_test.go
+++ problems/步长精度_test.go
@@ -14,9 +14,9 @@ func TestPrice(t *testing.T) {
 
 	// 向下取整
 	roundedDown := price.Div(step).Floor().Mul(step)
-	fmt.Printf("向下取整结果: %s\n", roundedDown.StringFixed(7))
+	fmt.Printf("向下取整结果: %s\n", roundedDown)
 
 	// 向上取整
 	roundedUp := price.Div(step).Ceil().Mul(step)
-	fmt.Printf("向上取整结果: %s\n", roundedUp.StringFixed(7))
+	fmt.Printf("向上取整结果: %s\n", roundedUp)
 }
