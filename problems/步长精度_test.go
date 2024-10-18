6b3459ba1925afe552ccd621696bd7175f7c8607
yant <yant@wanthings.com>
2024-10-18 11:28:30 +0800

    1

--- problems/步长精度_test.go
+++ problems/步长精度_test.go
@@ -9,8 +9,9 @@ import (
 
 func TestPrice(t *testing.T) {
 	// 定义价格和步长
-	price := decimal.NewFromFloat(0.1234567)
+	price := decimal.NewFromFloat(123456.1234567)
 	step := decimal.NewFromFloat(0.0001)
+	step2 := decimal.NewFromFloat(10)
 
 	// 向下取整
 	roundedDown := price.Div(step).Floor().Mul(step)
@@ -19,4 +20,14 @@ func TestPrice(t *testing.T) {
 	// 向上取整
 	roundedUp := price.Div(step).Ceil().Mul(step)
 	fmt.Printf("向上取整结果: %s\n", roundedUp)
+
+	fmt.Println("===================================")
+
+	// 向下取整
+	roundedDown2 := price.Div(step2).Floor().Mul(step2)
+	fmt.Printf("向下取整结果2: %s\n", roundedDown2)
+
+	// 向上取整
+	roundedUp2 := price.Div(step2).Ceil().Mul(step2)
+	fmt.Printf("向上取整结果2: %s\n", roundedUp2)
 }
